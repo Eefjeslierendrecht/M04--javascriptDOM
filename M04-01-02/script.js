6e31a6e1b347dec43fc30085a75babda74e24508
Eefjeslierendrecht <64635639+Eefjeslierendrecht@users.noreply.github.com>
2020-07-14 09:07:04 +0200

    Add files via upload

---
+++ M04-01-02/script.js
@@ -0,0 +1,30 @@
+// zet de functie om naar een arrow functie
+// const ikRockArrowFunctions = function () {
+//     console.log("Joe, ik rock de arrow functions!");
+// };
+
+// ikRockArrowFunctions()
+
+// Deel A Bouw de functie om naar een arrow functie
+let ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions")
+
+// Deel B Onderstaande functie omschrijven naar een arrow functie op 1 regel
+// const fivePlusSeven = function () {
+//     return 5 + 7
+//   };
+
+//   fivePlusSeven();
+
+let fivePlusSeven2 = () => 5 + 7;
+
+// Deel C zo kort mogelijke functie die 1+2 bij elkaar op telt
+count => 1 + 2
+
+// Deel D 
+let counting = myFunction (a, b) => a+b
+
+// Deel E
+let counting2 = addFive => a + 5
+
+// Deel F
+let objecten = createObject => console.log("hoi")
